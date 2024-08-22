44b3e7ffb3a11d3cb287c8e0329779a1aa597e22
laetuz <laetuzg@gmail.com>
2024-08-22 12:47:27 +0700

    Changelog: - Fixed nasty bug that can't save values from NeoTextField. - Added labels on NeoTextField. - updated to version 1.2.4

--- droidcore/src/main/java/id/neotica/droidcore/component/textfield/NeoTextField.kt
+++ droidcore/src/main/java/id/neotica/droidcore/component/textfield/NeoTextField.kt
@@ -3,14 +3,13 @@ package id.neotica.droidcore.component.textfield
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -18,38 +17,43 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NeoTextField(
-    value: String,
+    value: MutableState<String>,
     onValueChange: ((String) -> Unit)? = null,
     icon: (@Composable () -> Unit)? = null,
     placeHolder: String? = null,
     visualTransformation: VisualTransformation = VisualTransformation.None,
     isError: Boolean? = false,
     trailingIcon: (@Composable () -> Unit)? = null,
+    label: String? = null
 ) {
-    var textFieldValue by remember { mutableStateOf(value) }
-    if (onValueChange == null) {
-        OutlinedTextField(
-            value = textFieldValue,
-            onValueChange =  {
-                textFieldValue = it
-            },
-            leadingIcon = icon,
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = MaterialTheme.colorScheme.primary,
-                unfocusedBorderColor = Color.LightGray,
-            ),
-            placeholder = { Text(placeHolder?: "") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .heightIn(min = 48.dp),
-            shape = RoundedCornerShape(10.dp),
-            visualTransformation = visualTransformation,
-            isError = isError?: false,
-            trailingIcon = trailingIcon,
-            maxLines = 1
-        )
-    }
+    var textFieldValue by remember { value }
+
+    OutlinedTextField(
+        value = textFieldValue,
+        onValueChange =  {
+            textFieldValue = it
+            onValueChange?.invoke(it)
+        },
+        leadingIcon = icon,
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = MaterialTheme.colorScheme.primary,
+            unfocusedBorderColor = Color.LightGray,
+        ),
+        placeholder = { Text(placeHolder?: "") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 48.dp),
+        shape = RoundedCornerShape(10.dp),
+        visualTransformation = visualTransformation,
+        isError = isError == true,
+        trailingIcon = trailingIcon,
+        maxLines = 1,
+        label = {
+            if (label != null) {
+                Text(text = label)
+            }
+        }
+    )
 }
